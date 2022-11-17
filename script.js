cf01ade8eb8a569cdb5dfbcc083bf18c9bc16d0b
KiyomiDev <kiyomi.dev@gmail.com>
2022-11-17 21:22:35 +0200

    Create level selection functionality

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+const levelsContainer = document.querySelector('.select-level');
+const levelsBox = document.querySelectorAll('.level');
+const header = document.querySelector('.header');
+const mainSection = document.querySelector('main');
+const score = document.querySelector('.score span');
+const numberBetween = document.querySelector('.number-between');
+let max, selectedLevel, selectedLevelScore;
+
+levelsBox.forEach(level => {
+  level.addEventListener('click', () => {
+    max = Number(level.dataset.max);
+    selectedLevel = level.dataset.level;
+
+    setLevelScore(selectedLevel);
+
+    levelsContainer.classList.add('hide-slow');
+
+    setTimeout(() => {
+      levelsContainer.classList.add('hide');
+      header.classList.remove('hide');
+      mainSection.classList.remove('hide');
+    }, 600);
+
+    numberBetweenText(max);
+  })
+});
+
+const numberBetweenText = (max) => numberBetween.textContent = `(Between 1 and ${max})`;
+
+function setLevelScore(selectedLevel) {
+  if (selectedLevel === 'easy') selectedLevelScore = 20;
+  else if (selectedLevel === 'medium') selectedLevelScore = 25;
+  else selectedLevelScore = 30;
+  score.textContent = selectedLevelScore;
+}
